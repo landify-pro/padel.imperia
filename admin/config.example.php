ebd63a22f87cf7f570df9c2d5fe7d7c9c79ddd23
landify <nsk.yurt@gmail.com>
2026-09-05 02:23:18 +0700

    Add safe admin config template

---
+++ admin/config.example.php
@@ -0,0 +1,5 @@
+<?php
+// Copy this file to config.php on Beget and set your own credentials.
+$ADMIN_SALT = 'CHANGE_ME_TO_RANDOM_SALT';
+$ADMIN_PASSWORD_HASH = 'CHANGE_ME_TO_SHA256_HASH';
+$MAX_UPLOAD_BYTES = 5 * 1024 * 1024;
